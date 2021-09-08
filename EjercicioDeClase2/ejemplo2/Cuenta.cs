e653e6bf656a3c4a3916f4c59a65938bb2a91733
PavonEvelin <evelinluz17@gmail.com>
2021-09-08 17:42:54 -0300

    avance

---
+++ EjercicioDeClase2/ejemplo2/Cuenta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo2
+{
+    enum tipoDeExtraccion
+    {
+        cajeroHumano = 1,
+        cajeroAutomatico = 2
+    }
+    class Cuenta
+    {
+        private float monto;
+
+        public float Monto { get => monto; set => monto = value; }
+
+        public void insercion(int monto)
+        {
+
+        }
+
+        public void extraccion(int monto, tipoDeExtraccion tipo)
+        {
+
+        }
+    }
+}
